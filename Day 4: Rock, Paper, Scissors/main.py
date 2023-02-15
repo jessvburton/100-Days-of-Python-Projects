5a7cc6bf766ca82824b4c7e7a804f8c75b7fd435
jessvburton <93324273+jessvburton@users.noreply.github.com>
2023-02-15 16:03:14 +0000

    Create main.py

---
+++ Day 4: Rock, Paper, Scissors/main.py
@@ -0,0 +1,55 @@
+import random
+import art
+
+# Intro
+print("Welcome to a game of rock, paper, scissors")
+
+# Users input
+player1 = input("What do you choose? 'Rock', 'Paper', or 'Scissors'\n").lower()
+print(f"You chose {player1}!\n")
+if player1 == "rock":
+  print(art.r)
+elif player1 == "paper":
+  print(art.p)
+elif player1 == "scissors":
+  print(art.s)
+else:
+  print("Invalid input, try again.")
+
+# Computer response
+options = ["rock", "paper", "scissors"]
+cp = random.choice(options)
+print(f"Computer chose {cp}!")
+
+if cp == "rock":
+  print(art.r)
+elif cp == "paper":
+  print(art.p)
+elif cp == "scissors":
+  print(art.s)
+
+# Draw results
+if player1 == "rock" and cp == "rock":
+  print("It's a draw...")
+if player1 == "paper" and cp == "paper":
+  print("It's a draw...")
+if player1 == "scissors" and cp == "scissors":
+  print("It's a draw...")  
+
+# Rock wins
+if player1 == "rock" and cp == "scissors":
+  print("You win!")
+if player1 == "scissors" and cp == "rock":
+  print("You lose!")
+
+# Paper wins
+if player1 == "paper" and cp == "rock":
+  print("You win!")
+if player1 == "rock" and cp == "paper":
+  print("You lose!")
+
+# Scissors win
+if player1 == "scissors" and cp == "paper":
+  print("You win!")
+if player1 == "paper" and cp == "scissors":
+  print("You lose!")
