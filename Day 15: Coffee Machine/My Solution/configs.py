bb48249d336d82bf3045cec9ffd7e4e3167d37d6
Jessica Burton <93324273+jessvburton@users.noreply.github.com>
2023-09-27 11:28:56 +0100

    Create configs.py

---
+++ Day 15: Coffee Machine/My Solution/configs.py
@@ -0,0 +1,39 @@
+MENU = {
+    "espresso": {
+        "ingredients": {
+            "water": 50,
+            "coffee": 18,
+        },
+        "cost": 1.5,
+    },
+    "latte": {
+        "ingredients": {
+            "water": 200,
+            "milk": 150,
+            "coffee": 24,
+        },
+        "cost": 2.5,
+    },
+    "cappuccino": {
+        "ingredients": {
+            "water": 250,
+            "milk": 100,
+            "coffee": 24,
+        },
+        "cost": 3.0,
+    }
+}
+
+COIN_VALUES = {
+    "quarter": 0.25,
+    "dime": 0.10,
+    "nickle": 0.05,
+    "penny": 0.01
+}
+
+resources = {
+    "water": 300,
+    "milk": 200,
+    "coffee": 100,
+    "money": 0
+}
