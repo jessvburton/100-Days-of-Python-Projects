02f47820027dc66daa7c1a14c3b7c45653449f10
Jessica Burton <93324273+jessvburton@users.noreply.github.com>
2023-11-28 13:21:45 +0000

    Update main.py

--- Day 18: Turtle & the Graphical User Interface (GUI)/Draw a Square/main.py
+++ Day 18: Turtle & the Graphical User Interface (GUI)/Draw a Square/main.py
@@ -1,1 +1,12 @@
+import turtle
 
+tim = turtle.Turtle()
+tim.shape("turtle")
+tim.color("DeepPink")
+
+for i in range(4):
+    tim.fd(100)
+    tim.right(90)
+
+screen = turtle.Screen
+turtle.exitonclick()
