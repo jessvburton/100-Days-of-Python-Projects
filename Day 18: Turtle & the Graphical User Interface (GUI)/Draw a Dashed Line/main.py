ca03515df4a2eff63619531d3eb527d3f1abaea3
Jessica Burton <93324273+jessvburton@users.noreply.github.com>
2023-11-28 13:54:37 +0000

    Update main.py

--- Day 18: Turtle & the Graphical User Interface (GUI)/Draw a Dashed Line/main.py
+++ Day 18: Turtle & the Graphical User Interface (GUI)/Draw a Dashed Line/main.py
@@ -1,1 +1,13 @@
+from turtle import *
 
+tim = Turtle()
+tim.shape("turtle")
+
+for i in range(15):
+    tim.forward(10)
+    tim.penup()
+    tim.forward(10)
+    tim.pendown()
+
+screen = Screen
+exitonclick()
