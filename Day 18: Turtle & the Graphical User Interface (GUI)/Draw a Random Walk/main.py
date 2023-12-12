0cb35a074877c37038998d48a0488c5ae5573674
Jessica Burton <93324273+jessvburton@users.noreply.github.com>
2023-12-12 13:51:28 +0000

    Update main.py

--- Day 18: Turtle & the Graphical User Interface (GUI)/Draw a Random Walk/main.py
+++ Day 18: Turtle & the Graphical User Interface (GUI)/Draw a Random Walk/main.py
@@ -1,1 +1,19 @@
+from turtle import *
+from random import *
 
+pensize(10)
+speed("fast")
+colormode(255)
+directions = [0, 90, 180, 270]
+
+
+def change_colour():
+    pencolor(randint(0, 255),
+             randint(0, 255),
+             randint(0, 255))
+
+
+while True:
+    change_colour()
+    forward(50)
+    setheading(choice(directions))
