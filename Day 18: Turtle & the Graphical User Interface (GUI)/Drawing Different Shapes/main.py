252f8a8fdcc1adf90ca248a001a7f4eb44d0e588
Jessica Burton <93324273+jessvburton@users.noreply.github.com>
2023-12-12 13:10:34 +0000

    Update main.py

--- Day 18: Turtle & the Graphical User Interface (GUI)/Drawing Different Shapes/main.py
+++ Day 18: Turtle & the Graphical User Interface (GUI)/Drawing Different Shapes/main.py
@@ -1,1 +1,23 @@
+from turtle import *
+from random import randint
 
+colormode(255)
+shapes = {"triangle": 3, "square": 4, "pentagon": 5, "hexagon": 6, "heptagon": 7, "octagon": 8, "nonagon": 9,
+          "decagon": 10}
+
+
+def change_colour():
+    pencolor(randint(0, 255),
+             randint(0, 255),
+             randint(0, 255))
+
+
+for i in shapes:
+    change_colour()
+    for _ in range(shapes[i]):
+        forward(100)
+        right(360/shapes[i])
+
+
+screen = Screen
+exitonclick()
