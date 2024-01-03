8d056d753f0478648fa61d5dff3df469b0878a80
Jessica Burton <93324273+jessvburton@users.noreply.github.com>
2024-01-03 10:21:33 +0000

    Update main.py

--- Day 18: Turtle & the Graphical User Interface (GUI)/Draw a Spirograph/main.py
+++ Day 18: Turtle & the Graphical User Interface (GUI)/Draw a Spirograph/main.py
@@ -1,1 +1,23 @@
+from turtle import *
+from random import randint
 
+
+def change_colour():
+    colormode(255)
+    pencolor(randint(0, 255),
+             randint(0, 255),
+             randint(0, 255))
+
+
+def draw_spirograph():
+    circles = int(input("How many circles would you like?\n"))
+    angle = 360 / circles
+    for _ in range(circles):
+        change_colour()
+        circle(100)
+        left(angle)
+
+
+speed("fastest")
+draw_spirograph()
+exitonclick()
